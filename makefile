e8a6316ad5cdebbf3e987a79f544d1725cff1629
Matéo <mateocheveau2109@gmail.com>
2026-02-26 23:43:09 +0100

    test GUI

--- makefile
+++ makefile
@@ -1,6 +1,7 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -std=c99
-SRC = src/*
+CFLAGS = -Wall -Wextra -std=c99 -I./include
+LDFLAGS = -L./lib -lraylib -lm -lgdi32 -lwinmm
+SRC = $(wildcard src/*.c)
 OBJ = $(SRC:src/%.c=bin/%.o)
 BIN_DIR = bin
 TARGET = $(BIN_DIR)/graphe
@@ -11,7 +12,7 @@ $(BIN_DIR):
 	mkdir -p $(BIN_DIR)
 
 $(TARGET): $(OBJ)
-	$(CC) $(CFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 bin/%.o: src/%.c src/%.h
 	$(CC) $(CFLAGS) -c $< -o $@
--- src/controller.c
+++ src/controller.c
@@ -3,11 +3,31 @@
 
 #include "graphe.h"
 #include "solveur.h"
+#include "raylib.h"
+
+void fenetre()
+{
+    InitWindow( 1280, 960, "Graphe" );
+    SetTargetFPS(60);
+    Color background = { 255, 255, 255, 255 };
+
+    while ( !WindowShouldClose() )
+    {
+        PollInputEvents();
+        BeginDrawing();
+        EndDrawing();
+        ClearBackground( background );
+    }
+    
+    CloseWindow();
+}
 
 int main( int argc, char *argv[] )
 {
     if ( argc != 2 ) { return -1; }
 
+    fenetre();
+    
     char *texte = getContenuFichier( argv[1] );
     if ( texte == NULL ) { return -1; }
 
